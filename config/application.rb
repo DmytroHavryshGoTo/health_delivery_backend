4900148802bbdfb94c0ef94b8d13db90655e2817
Vladimir Mozgin <vladimir.mozgin@gotoinc.co>
2021-10-26 19:58:53 +0300

    Fix time zone

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module HealthDeliveryBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Europe/Kiev'
 
     # Configuration for the application, engines, and railties goes here.
     #
